6e4ef06a31a8fd87374e4ebc20b2fb5d379435ef
Alaa Soleiman <aasoleiman@Netways.com>
2025-07-14 09:29:05 +0300

    in progres s

--- app/(screens)/index.tsx
+++ app/(screens)/index.tsx
@@ -1,71 +1,29 @@
-import CustomText from "@/app/components/CustomText/CustomText";
-import BluryOrangeBackground from "@/assets/images/bluryOrangeBackground.png";
-import location from "@/assets/images/location.png";
-import React from "react";
-import {
-  Dimensions,
-  Image,
-  ImageBackground,
-  StyleSheet,
-  View,
-} from "react-native";
+import OnBoarding from "@/app/(screens)/OnBoarding";
+import LoadingScreen from "@/app/components/LoadingScreen/LoadingScreen";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-const { height, width } = Dimensions.get("window");
+export default function Index() {
+  const [isLoading, setIsLoading] = useState(true);
 
-const SelectLocation = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={BluryOrangeBackground}
-        style={styles.backgroundImage}
-      >
-        <Image source={location} style={styles.locationImage} />
-      </ImageBackground>
-
-      <View style={styles.selectLocationTextContainer}>
-        <CustomText style={styles.selectLocationText}>
-          Select Your Location
-        </CustomText>
-        <CustomText style={styles.selectLocationSubText}>
-          Swithch on your location to stay in tune with what’s happening in your
-          area
-        </CustomText>
-      </View>
-
-      <View>
-        <CustomText style={styles.label}>Code</CustomText>
-      </View>
+      {isLoading ? <LoadingScreen /> : <OnBoarding />}
     </View>
   );
-};
-
-export default SelectLocation;
+}
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#FCFCFC",
     flex: 1,
-  },
-  backgroundImage: {
-    paddingHorizontal: width * 0.06,
-    height: height * 0.25,
-  },
-  locationImage: {
-    alignSelf: "center",
-    marginTop: height * 0.15,
-    width: width * 0.6,
-  },
-  selectLocationTextContainer: {
+    backgroundColor: "#53B175",
+    justifyContent: "center",
     alignItems: "center",
-    marginTop: height * 0.15,
-    paddingHorizontal: width * 0.06,
   },
-  selectLocationText: { textAlign: "center", fontSize: 28, color: "#181725" },
-  selectLocationSubText: {
-    textAlign: "center",
-    fontSize: 16,
-    color: "#7C7C7C",
-    marginTop: 15,
-  },
-  label: {}
 });
